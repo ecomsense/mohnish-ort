c973a54634b66eec75a1b61db814f22f2ae4bdb3
pannet1 <prog@ecomsense.in>
2024-06-26 01:27:31 +0530

    first commit

---
+++ src/main.py
@@ -0,0 +1,19 @@
+from constants import logging, O_CNFG, O_SETG, O_FUTL
+
+
+def run():
+    """
+    repetitive tasks that will
+    run again and again
+    """
+    raise Exception("Not Implemented")
+
+
+def main():
+    logging.info("HAPPY TRADING")
+    """
+    description:
+        initialize singtons here
+        also declare globals if any here
+    """
+    run()
